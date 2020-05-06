99757105568b8a338f08f27a8edbe74fa3c92b33
harturalcantara <harturalcantara@gmail.com>
2020-05-06 20:19:54 -0300

    update

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import './App.css'
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import ComFilhos from './components/basicos/ComFilhos'
 import Card from './components/layout/Card'
-import Repeticao from './components/Repeticao'
-import Condicional from './components/Condicional'
-import CondicionalComIf from './components/CondicionalComIf'
+import Repeticao from './components/basicos/Repeticao'
+import Condicional from './components/basicos/Condicional'
+import CondicionalComIf from './components/basicos/CondicionalComIf'
+import Pai from './components/comunicacao/direta/Pai'
+import Super from './components/comunicacao/indireta/Super'
 
 export default (props) => (
     <div className="App">
-        <Card titulo="#06 - Condicional v2">
+        <Card titulo="#08 - Comunicação Indireta" color="#000">
+            <Super></Super>
+        </Card>
+        <Card titulo="#07 - Comunicação Direta" color="#4298B5">
+            <Pai sobrenome="Barreto"></Pai>
+        </Card>
+        <Card titulo="#06 - Condicional v2" color="#FA6900">
             <CondicionalComIf numero={72}></CondicionalComIf>
         </Card>
-        <Card titulo="#05 - Condicional v1">
+        <Card titulo="#05 - Condicional v1" color="#E94C6F">
             <Condicional numero={11}></Condicional>
         </Card>
-        <Card titulo="#04 - Repetição">
+        <Card titulo="#04 - Repetição" color="#008BBA">
             <Repeticao></Repeticao>
         </Card>
         
-        <Card titulo="#03 - Componente com Filhos">
+        <Card titulo="#03 - Componente com Filhos" color="#D96459">
             <ComFilhos>
                 <ul>
                     <li>Ana</li>
@@ -32,12 +40,12 @@ export default (props) => (
             </ComFilhos>
         </Card>
         
-        <Card titulo="#02 - Componente com Parametro">
+        <Card titulo="#02 - Componente com Parametro" color="#FF85CB">
             <ComParametro titulo="Esse é o titulo" subtitulo="Esse é o subtitulo" />
             <ComParametro titulo="OPA!" subtitulo="EPA!" />
         </Card>
 
-        <Card titulo="#01 - Primeiro Componente"> 
+        <Card titulo="#01 - Primeiro Componente" color="#92B06A"> 
             <Primeiro /> 
         </Card>
 
---
+++ fundamentos-react/src/components/comunicacao/indireta/Super.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sub from './Sub'
+
+export default (props) => {
+
+    function quandoClicar(valorGerado){
+        console.log("Ação!!!");
+        console.log(valorGerado);
+    }
+
+    return (
+        <div>
+            <h4> Valor </h4>
+            <Sub onClicar={quandoClicar}></Sub>
+        </div>
+    );
+};
+    
